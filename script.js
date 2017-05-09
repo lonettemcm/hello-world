ff620c801bc839024f4c171fee55c325eaf3e2ca
lonettemcm <lonettemcm@gmail.com>
2017-05-09 15:42:04 -0500

    Update script.js

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ var nounTwo = prompt('Please name your last noun' + questionsLeft);
 
 alert('Congrats! You made it. Ready for the message?');
 var sentence = '<h2>Meet our hero ' + sillyName + ', a super intelligent ' + profession + '. ';
-sentence += 'A run-in with the country ' + country + ' leads him to create his alter ego ' + sillyNameTwo + ' a ' + color + ' ' + adjective + ' giant capable of great destruction.;
+sentence += 'A run-in with the country ' + country + ' leads him to create his alter ego ' + sillyNameTwo + ' a ' + color + ' ' + adjective + ' giant capable of great destruction.';
 sentence += ' He ' + adverb + ' battles the military with his girlfriend ' + sillyNameThree + '. ';
 sentence += 'Eventually it is discovered that our hero\'s long-time colleague ' + sillyNameFour + ', distinguished by his ' + face + ' is trying to turn ' + sillyNameFive + ' into a weapon, leading to a climactic (if pointless) battle in downtown ' + city + ' with an evil version of the same giant alter-ego called ' + sillyNameSix + '. ';
 sentence += 'Eventually the enemy is subdued by ' + verb + 'ing him with a ' + noun + '. in the final reel, ' + actor + ' appears to propose joining him in a ' + nounTwo + '.';
